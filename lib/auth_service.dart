bec864cc276e12e773cc647ffee9bddb6d0bfb82
Pratham Sarankar <pratham.sarankar@yahoo.com>
2021-08-09 12:45:01 +0530

    init

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -6,7 +6,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 // import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class AuthService {
-  static Stream<User?> authStream() {
+  static Stream<User?> userStream() {
     return FirebaseAuth.instance.authStateChanges();
   }
 
